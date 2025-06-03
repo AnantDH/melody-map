3d6c3fd2923fb5dd7dca3ec02c101817d0dfacf9
anantd <anantd@uw.edu>
2025-06-03 16:20:16 -0700

    Initial Commit

---
+++ MelodyMap/MelodyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodyMapApp.swift
+//  MelodyMap
+//
+//  Created by Anant Dhokia on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MelodyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MelodyMapTests/MelodyMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MelodyMapTests.swift
+//  MelodyMapTests
+//
+//  Created by Anant Dhokia on 6/3/25.
+//
+
+import Testing
+@testable import MelodyMap
+
+struct MelodyMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
